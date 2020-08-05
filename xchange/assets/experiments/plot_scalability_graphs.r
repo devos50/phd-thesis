d440d923fe26df377eb748232b373c7d6074eaea
Martijn de Vos <mdmartijndevos@gmail.com>
2020-08-05 17:08:08 +0200

    Added latest XChange paper

--- xchange/assets/experiments/plot_scalability_graphs.r
+++ xchange/assets/experiments/plot_scalability_graphs.r
@@ -2,14 +2,14 @@ library(ggplot2)
 library(dplyr)
 
 #cols <- c("NOCHECK" = "#db0017", "CHECK" = "#136ac6", "CHECK+LIMTRADE" = "#40a33a")
-linetypes <- c("none" = "solid", "RESTRICT(1)" = "longdash", "INC_SET(2)" = "dashed", "RESTRICT(1)+INC_SET(2)" = "twodash")
-dotstyles <- c("none" = 15, "RESTRICT(1)" = 16, "INC_SET(2)" = 17, "RESTRICT(1)+INC_SET(2)" = 18)
-order <- c("none","RESTRICT(1)","INC_SET(2)", "RESTRICT(1)+INC_SET(2)")
+linetypes <- c("none" = "solid", "RESTRICT" = "longdash", "INC_SET(2)" = "dashed", "RESTRICT+INC_SET(2)" = "twodash")
+dotstyles <- c("none" = 15, "RESTRICT" = 16, "INC_SET(2)" = 17, "RESTRICT+INC_SET(2)" = 18)
+order <- c("none","RESTRICT","INC_SET(2)", "RESTRICT+INC_SET(2)")
 
 dat <- read.csv("scalability.csv")
 #dat$avg_bw <- dat$bw / dat$network_size / 1024.0
 #dat$avg_trades <- dat$trades / 30.0
-dat$system_load <- dat$instances * 2
+dat$system_load <- dat$instances
 
 # Average the results
 dat <- dat %>%
